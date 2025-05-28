667900bbbc635a9452f7c682cd0a6f29684cd15b
Pablo Reble <pablo.reble@intel.com>
2025-05-28 13:33:46 -0700

    Apply suggestions from review

--- adoc/code/graph.cpp
+++ adoc/code/graph.cpp
@@ -1,5 +1,9 @@
+// Copyright (c) 2025 The Khronos Group, Inc.
+// SPDX-License-Identifier: MIT
+
 #include <iostream>
 #include <sycl/sycl.hpp>
+#include <utility>
 using namespace sycl;  // (optional) avoids need for "sycl::" before SYCL names
 
 constexpr size_t N = 100;
@@ -10,48 +14,52 @@ int main() {
   queue myQueue({property::queue::in_order{}});
 
   // Create some 1D arrays of float
-  float* A_old = malloc_device<float>(N, myQueue);
-  float* A_new = malloc_device<float>(N, myQueue);
-    
-  {
-    khr::command_graph myGraph{myQueue};
+  float* AOld = malloc_device<float>(N, myQueue);
+  float* ANew = malloc_device<float>(N, myQueue);
 
-    myGraph.begin_recording(myQueue);
+  khr::command_graph myGraph{myQueue};
 
-    // Record an asynchronous kernel to compute even iterations
-    myQueue.submit([&](handler& cgh) {
-      // Enqueue a parallel kernel iterating on a N 1D iteration space
-      cgh.parallel_for(range<1>{N-2}, [=](id<1> index) {
-        A_new[index+1] = A_old[index] / 2 + A_old[index+2] / 2;
-      });
+  myGraph.begin_recording(myQueue);
+
+  // Record an asynchronous kernel to compute even iterations
+  myQueue.submit([&](handler& cgh) {
+    // Enqueue a parallel kernel iterating on a N 1D iteration space
+    cgh.parallel_for(range<1>{N - 2}, [=](id<1> index) {
+      ANew[index + 1] = AOld[index] / 2 + AOld[index + 2] / 2;
     });
+  });
 
-    std::swap(A_old, A_new);
+  std::swap(AOld, ANew);
 
-    // Record an asynchronous kernel to compute odd iterations
-    myQueue.submit([&](handler& cgh) {
-      // Enqueue a parallel kernel iterating on a N 1D iteration space
-      cgh.parallel_for(range<1>{N-2}, [=](id<1> index) {
-        A_new[index+1] = A_old[index] / 2 + A_old[index+2] / 2;
-      });
+  // Record an asynchronous kernel to compute odd iterations
+  myQueue.submit([&](handler& cgh) {
+    // Enqueue a parallel kernel iterating on a N 1D iteration space
+    cgh.parallel_for(range<1>{N - 2}, [=](id<1> index) {
+      ANew[index + 1] = AOld[index] / 2 + AOld[index + 2] / 2;
     });
+  });
 
-    myGraph.end_recording();
+  myGraph.end_recording();
 
-    khr::command_graph<khr::graph_state::executable> myExecGraph =
-        myGraph.finalize();
+  khr::command_graph<khr::graph_state::executable> myExecGraph =
+      myGraph.finalize();
 
-    // Replay linear graph which holds two iterations
-    for(size_t i = 0; i < Iter; i+=2)
-      myQueue.khr_graph(myExecGraph);
-      
-    myQueue.wait();
-  }
+  // Initialize data on device
+  myQueue.submit([&](handler& cgh) {
+    cgh.single_task([=]() {
+      ANew[0] = ANew[N - 1] = AOld[0] = AOld[N - 1] = 100.0f;
+      for (size_t i = 1; i < N - 1; i++) AOld[i] = 0.0f;
+    });
+  });
+
+  // Replay linear graph which holds two iterations
+  for (size_t i = 0; i < Iter; i += 2) myQueue.khr_graph(myExecGraph);
+
+  myQueue.wait();
 
   std::cout << "Success!" << std::endl;
 
-  free(A_old, myQueue);
-  free(A_new, myQueue);
+  free(AOld, myQueue);
+  free(ANew, myQueue);
   return 0;
 }
-
